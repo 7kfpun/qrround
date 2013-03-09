f7df3340c8915969312631b5dbe525893f35bea3
kf <7kfpun@gmail.com>
2013-03-09 09:11:45 +0800

    add bootstrap

--- qrround/qrround/settings.py
+++ qrround/qrround/settings.py
@@ -2,6 +2,8 @@
 import os
 
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -74,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -108,8 +111,6 @@ ROOT_URLCONF = 'qrround.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'qrround.wsgi.application'
-
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
