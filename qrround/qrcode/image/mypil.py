4382b837e7cbc0610530f38d0b36de8e2ddb7c8b
kf <7kfpun@gmail.com>
2013-03-13 10:43:01 +0800

    relocate png file

--- qrround/qrcode/image/mypil.py
+++ qrround/qrcode/image/mypil.py
@@ -27,7 +27,7 @@ class PilImage(qrcode.image.base.BaseImage):
         # self._img = Image.new("1", (pixelsize, pixelsize), "white")
         self._idr = ImageDraw.Draw(self._img)
 
-        # url = "https://secure.gravatar.com/avatar/988f8daaaf0155e5536fbb2d7efe0d0f?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
+        # url = "https://secure.gravatar.com/avatar/988f8daaaf0155e5536fbb2d7efe0d0f?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"  # noqa
         # self._image = Image.open(StringIO(urllib.urlopen(url).read()))
         self._image = Image.open("wing.png")
         self._image = self._image.resize(
@@ -36,18 +36,18 @@ class PilImage(qrcode.image.base.BaseImage):
         self._all_cached_images = CachedImage.objects.all()
         if not self._all_cached_images:
             self._all_cached_images = [
-                Image.open("a.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
-                Image.open("b.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
-                Image.open("c.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
-                Image.open("d.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
-                Image.open("e.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
-                Image.open("f.png").resize((self.box_size, self.box_size),
-                                           Image.ANTIALIAS),
+                Image.open("qrround/media/a.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
+                Image.open("qrround/media/b.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
+                Image.open("qrround/media/c.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
+                Image.open("qrround/media/d.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
+                Image.open("qrround/media/e.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
+                Image.open("qrround/media/f.png").resize(
+                    (self.box_size, self.box_size), Image.ANTIALIAS),
             ]
 
     def drawrect(self, row, col):
@@ -65,7 +65,7 @@ class PilImage(qrcode.image.base.BaseImage):
         image = choice(self._all_cached_images)
         self._img.paste(Image.open(image.photo.path).resize(
             (self.box_size, self.box_size), Image.ANTIALIAS), (x, y))
-            
+
     def show(self):
         self._img.show()
 
