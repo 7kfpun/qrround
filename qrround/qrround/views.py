d94b7cf8e14c0d8b72e577c71e558a707f9d1afb
kf <7kfpun@gmail.com>
2013-03-13 12:27:04 +0800

    add get auth without js sdk

--- qrround/qrround/views.py
+++ qrround/qrround/views.py
@@ -1,6 +1,6 @@
 from django.core.files import File
 from django.db import transaction
-from django.http import HttpResponse
+from django.http import HttpResponse  # , HttpResponseRedirect
 from django.shortcuts import render  # , get_object_or_404
 from django.template import Context, Template
 from helpers import unique_generator
@@ -16,6 +16,7 @@ from qrround.models import (
     CachedImage,
 )
 from ratelimit.decorators import ratelimit
+from rauth import OAuth1Service, OAuth2Service
 from settings.settings import MEDIA_ROOT
 #import StringIO
 #import tweepy
@@ -35,12 +36,53 @@ def index(request):
 #        twitter_auth_url = None
 #        logger.error('Error! Failed to get request token.')
 
+    # Protect against Cross-Site Request Forgery
+    # STATE = 'YOUR_STATE_VALUEYOUR_STATE_VALUEYOUR_STATE_VALUE'
+
+    facebook = OAuth2Service(
+        client_id='236929692994329',
+        client_secret='9d65f7d0069567d6958f559ad918ada7',
+        name='facebook',
+        authorize_url='https://graph.facebook.com/oauth/authorize',
+        access_token_url='https://graph.facebook.com/oauth/access_token',
+        base_url='https://graph.facebook.com/')
+    redirect_uri = 'http://127.0.0.1:8001/facebook_callback'
+    params = {'scope': 'read_stream,publish_actions',
+              'response_type': 'code',
+              'redirect_uri': redirect_uri}
+
+    facebook_auth_url = facebook.get_authorize_url(**params)
+
+    facebook_auth_url = (
+        'https://www.facebook.com/dialog/oauth/?'
+        'client_id=236929692994329'
+        '&redirect_uri=http://127.0.0.1:8001/facebook_callback'
+        '&state=STATE'
+        '&scope=read_stream,publish_actions'
+    )
+
+    twitter = OAuth1Service(
+        consumer_key='2Icic6DEGROMML9U3Xrrg',
+        consumer_secret='2T4a3MpeqGSgOAehVrpm6hIO7ymf88XNabZgdZi7M',
+        name='twitter',
+        access_token_url='https://api.twitter.com/oauth/access_token',
+        authorize_url='https://api.twitter.com/oauth/authorize',
+        request_token_url='https://api.twitter.com/oauth/request_token',
+        base_url='https://api.twitter.com/1/'
+    )
+    request_token = twitter.get_request_token()[0]
+
+    twitter_auth_url = twitter.get_authorize_url(
+        request_token,
+        callback_url='http://127.0.0.1:8001/twitter_callback'
+    )
+
     google_auth_url = (
         'https://accounts.google.com/o/oauth2/auth?'
         'scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email'
         '+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile'
-        '&redirect_uri=http://127.0.0.1:8001/oauth2callback&response_type=code'
-        '&client_id=533974579689.apps.googleusercontent.com'
+        '&redirect_uri=http://127.0.0.1:8001/google_callback&response_type=code'  # noqa
+        '&client_id=533974579689-j6h3lt2toobuok26n9o5g3n0qo0k2mbm.apps.googleusercontent.com'  # noqa
     )
 
     linkedin_auth_url = (
@@ -48,7 +90,7 @@ def index(request):
         '&client_id=2ykkt7cjhrcg'
         '&scope=r_basicprofile%20r_emailaddress%20r_network'
         '&state=STATE'
-        '&redirect_uri=http://127.0.0.1:8001/'
+        '&redirect_uri=http://127.0.0.1:8001/linkedin_callback'
     )
 
 #    if request.GET.get('oauth_verifier'):
@@ -60,10 +102,52 @@ def index(request):
 #            logger.error('Error! Failed to get access token.')
 
     return render(request, 'index.html', {
+        'facebook_auth_url': facebook_auth_url,
         'google_auth_url': google_auth_url,
-        'twitter_auth_url': None,
         'linkedin_auth_url': linkedin_auth_url,
+        'twitter_auth_url': twitter_auth_url,
     })
+
+
+def facebookcallback(request):
+    response = HttpResponse('facebook_auth_session:' + json.dumps(request.GET))
+    response.set_cookie('facebook_auth_session',
+                        request.GET.get('code'), max_age=1000)
+    return response
+
+
+def googlecallback(request):
+    response = HttpResponse('google_auth_session:' + json.dumps(request.GET))
+    response.set_cookie('google_auth_session',
+                        request.GET.get('code'), max_age=1000)
+    return response
+
+
+def linkedincallback(request):
+    response = HttpResponse('linkedin_auth_session:' + json.dumps(request.GET))
+    response.set_cookie('linkedin_auth_session',
+                        request.GET.get('code'), max_age=1000)
+    return response
+
+
+def twittercallback(request):
+    response = HttpResponse('twitter_auth_session:' + json.dumps(request.GET))
+    response.set_cookie('twitter_auth_session',
+                        request.GET.get('oauth_token'), max_age=1000)
+    return response
+
+
+def oauth2callback(request):
+    return HttpResponse(request.GET.get('code'))
+
+
+def close_window(request, is_reload=False):
+    url = "window.opener.document.location.href"
+    reload_line = "window.opener.document.location.href = %s;" % url \
+        if is_reload else ""
+    html = '''<script type="text/javascript">
+           %swindow.close();</script>''' % reload_line
+    return HttpResponse(html)
 
 
 @ratelimit(rate='20/m')
@@ -117,10 +201,6 @@ def getqrcode(request):
 
         except IndexError, e:
             return HttpResponse(e)
-
-
-def oauth2callback(request):
-    return HttpResponse(request.GET.get('code'))
 
 
 @transaction.commit_on_success
