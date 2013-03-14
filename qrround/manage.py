d432c503a9c82d24a409eddf1aac5160ebb4fe02
kf <7kfpun@gmail.com>
2013-03-14 17:11:47 +0800

    add support for distringuish local/dotcloud

--- qrround/manage.py
+++ qrround/manage.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-from qrround.settings import dotcloud
+import os
 
 
 if __name__ == "__main__":
-    execute_manager(dotcloud)
+
+    if os.environ.get('PYTHONPATH', None) == '/home/dotcloud':
+        from django.core.management import execute_manager
+        from qrround.settings import dotcloud
+
+        execute_manager(dotcloud)
+
+    else:
+        from django.core.management import execute_from_command_line
+        import sys
+
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "qrround.settings.settings")
+        execute_from_command_line(sys.argv)
