edccff88812dc35d2e6c3909e7a88aa3d6f1eda2
kf <7kfpun@gmail.com>
2013-03-21 15:56:01 +0800

    move channal keys and secrets to channel_settings.py

--- qrround/qrround/channels.py
+++ qrround/qrround/channels.py
@@ -2,13 +2,15 @@ from oauth2client.client import OAuth2WebServerFlow  # for google
 from rauth import OAuth2Service
 import tweepy
 
+from .channels_settings import *  # noqa
+
 
 channels = ['facebook', 'google', 'linkedin',
-    'renren', 'twitter', 'weibo', 'kaixin001']
+            'renren', 'twitter', 'weibo', 'kaixin001']
 
 facebook = OAuth2Service(
-    client_id='236929692994329',
-    client_secret='9d65f7d0069567d6958f559ad918ada7',
+    client_id=FACEBOOK_CLIENT_ID,
+    client_secret=FACEBOOK_CLIENT_SECRET,
     name='facebook',
     authorize_url='https://graph.facebook.com/oauth/authorize',
     access_token_url='https://graph.facebook.com/oauth/access_token',
@@ -16,15 +18,15 @@ facebook = OAuth2Service(
 )
 
 google = OAuth2WebServerFlow(
-    client_id='533974579689-j6h3lt2toobuok26n9o5g3n0qo0k2mbm.apps.googleusercontent.com',  # noqa
-    client_secret='dtLZ9z-AGhid6knEOC54qudr',
+    client_id=GOOGLE_CLIENT_ID,  # noqa
+    client_secret=GOOGLE_CLIENT_SECRET,
     scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.me',  # noqa
-    redirect_uri='http://127.0.0.1:8001/google_callback',
+    redirect_uri=GOOGLE_REDIRECT_URI,
 )
 
 linkedin = OAuth2Service(
-    client_id='2ykkt7cjhrcg',
-    client_secret='TV7x10lw1JY6Zfe2',
+    client_id=LINKEDIN_CLIENT_ID,
+    client_secret=LINKEDIN_CLIENT_SECRET,
     name='linkedin',
     authorize_url='https://www.linkedin.com/uas/oauth2/authorization',
     access_token_url='https://www.linkedin.com/uas/oauth2/accessToken',
@@ -32,8 +34,8 @@ linkedin = OAuth2Service(
 )
 
 renren = OAuth2Service(
-    client_id='229108',
-    client_secret='8815838e95504011a18673ea9f37f3b4',
+    client_id=RENREN_CLIENT_ID,
+    client_secret=RENREN_CLIENT_SECRET,
     name='renren',
     authorize_url='https://graph.renren.com/oauth/authorize',
     access_token_url='https://graph.renren.com/oauth/authorize',
@@ -41,13 +43,13 @@ renren = OAuth2Service(
 )
 
 twitter = tweepy.OAuthHandler(
-    consumer_key="2Icic6DEGROMML9U3Xrrg",
-    consumer_secret="2T4a3MpeqGSgOAehVrpm6hIO7ymf88XNabZgdZi7M",
+    consumer_key=TWITTER_CONSUMER_KEY,
+    consumer_secret=TWITTER_CONSUMER_SECRET,
 )
 
 weibo = OAuth2Service(
-    client_id='1736274547',
-    client_secret='f6f8fa98288e0cb75d9fe291f14c33eb',
+    client_id=WEIBO_CLIENT_ID,
+    client_secret=WEIBO_CLIENT_SECRET,
     name='weibo',
     authorize_url='https://api.weibo.com/oauth2/authorize',
     access_token_url='https://api.weibo.com/oauth2/access_token',
@@ -56,8 +58,8 @@ weibo = OAuth2Service(
 # POST  https://api.weibo.com/2/statuses/update.json?access_token=2.00wQWhVDL1OVtB1375cc41d6BUNfYB status=status  # noqa
 
 kaixin001 = OAuth2Service(
-    client_id='1214876808351987b5b2f5659b72f67c',
-    client_secret='bf2726ad4eb6dc8e3c41fa6f9edf8ab3',
+    client_id=KAIXIN001_CLIENT_ID,
+    client_secret=KAIXIN001_CLIENT_SECRET,
     name='weibo',
     authorize_url='http://api.kaixin001.com/oauth2/authorize',
     access_token_url='https://api.kaixin001.com/oauth2/access_token',
--- qrround/qrround/channels_settings.py
+++ qrround/qrround/channels_settings.py
@@ -10,19 +10,33 @@ if local:
 
     GOOGLE_CLIENT_ID = '533974579689-j6h3lt2toobuok26n9o5g3n0qo0k2mbm.apps.googleusercontent.com'
     GOOGLE_CLIENT_SECRET = 'dtLZ9z-AGhid6knEOC54qudr'
-    GOOGLE_REDIRECT_URI 'http://127.0.0.1:8001/google_callback'
+    GOOGLE_REDIRECT_URI = 'http://127.0.0.1:8001/google_callback'
 
     LINKEDIN_CLIENT_ID = '2ykkt7cjhrcg'
     LINKEDIN_CLIENT_SECRET = 'TV7x10lw1JY6Zfe2'
     LINKEDIN_REDIRECT_URI = 'http://127.0.0.1:8001/linkedin_callback'
 
+    RENREN_CLIENT_ID = '229108'
+    RENREN_CLIENT_SECRET = '8815838e95504011a18673ea9f37f3b4'
+    RENREN_REDIRECT_URI = 'http://127.0.0.1:8001/renren_callback'
+
+    TWITTER_CONSUMER_KEY = '2Icic6DEGROMML9U3Xrrg'
+    TWITTER_CONSUMER_SECRET = '2T4a3MpeqGSgOAehVrpm6hIO7ymf88XNabZgdZi7M'
+
+    WEIBO_CLIENT_ID = '1736274547'
+    WEIBO_CLIENT_SECRET = 'f6f8fa98288e0cb75d9fe291f14c33eb'
+    WEIBO_REDIRECT_URI = 'http://127.0.0.1:8001/weibo_callback'
+
+    KAIXIN001_CLIENT_ID = '1214876808351987b5b2f5659b72f67c'
+    KAIXIN001_CLIENT_SECRET = 'bf2726ad4eb6dc8e3c41fa6f9edf8ab3'
+    KAIXIN001_REDIRECT_URI = 'http://127.0.0.1:8001/kaixin001_callback'
 
 else:
     FACEBOOK_REDIRECT_URI = 'http://qrround-710kfpun.dotcloud.com/facebook_callback'
 
     GOOGLE_CLIENT_ID = '533974579689.apps.googleusercontent.com'
     GOOGLE_CLIENT_SECRET = 'OJyFy0XPYsjtkuq6R7TBcILn'
-    GOOGLE_REDIRECT_URI 'http://qrround-710kfpun.dotcloud.com/google_callback'
+    GOOGLE_REDIRECT_URI = 'http://qrround-710kfpun.dotcloud.com/google_callback'
 
     LINKEDIN_CLIENT_ID = 'l22w31c7kwzf'
     LINKEDIN_CLIENT_SECRET = 'hJ8GiEDxcLZnVkUS'
