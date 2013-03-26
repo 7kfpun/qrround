2d7a7e97588f4b6b7b2bbc20257799b0999be0b8
kf <7kfpun@gmail.com>
2013-03-26 15:49:36 +0800

    fix thumbnail on dotcloud

--- qrround/qrround/settings/settings.py
+++ qrround/qrround/settings/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = 'media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = op.join(PROJECT_ROOT, 'qrround/static')
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
